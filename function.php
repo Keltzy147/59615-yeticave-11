9795dd683d6170302bb461a61644a7689ffa3a07
Vafin Marat <56917627+Keltzy147@users.noreply.github.com>
2019-11-22 09:31:56 +0300

    Сделано 6.1

---
+++ function.php
@@ -0,0 +1,41 @@
+<?php 
+function price(int $price) : string {
+	if (ceil($price) < 1000) {
+		return $price . " ₽";
+	}
+	else{
+		return number_format($price, 0, ',', ' ') . " ₽";
+	}
+}
+
+function include_template($name, array $data = []) {
+	$name = 'templates/' . $name;
+	$result = '';
+
+	if (!is_readable($name)) {
+		return $result;
+	}
+
+	ob_start();
+	extract($data);
+	require $name;
+
+	$result = ob_get_clean();
+
+	return $result;
+}
+
+function timer($time){
+	$diff = strtotime($time) - time();
+
+    $hours = floor($diff / 60 / 60); //  перевод в часы с округлением вниз
+    $hours = str_pad ($hours, 2, "0", STR_PAD_LEFT); // добавляем 0 перед числом, если число меньше 2 знаков
+
+    $minutes = floor(($diff - ($hours * 60 * 60)) / 60); // получаем минуты
+    $minutes = str_pad ($minutes, 2, "0", STR_PAD_LEFT); // добавляем 0 перед числом, если число меньше 2 знаков
+
+    $timer = $hours . ':' . $minutes;
+
+    return $timer;
+}
+?>
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 $is_auth = rand(0, 1);
 $user_name = "Марат"; // укажите здесь ваше имя
 
+require_once("function.php");
 $connect_db = mysqli_connect("localhost","root","","yeticave");
 
 if(!$connect_db){
@@ -21,7 +22,7 @@ else{
     }
 
     $sql_products = <<<SQL
-SELECT lots.name, lots.first_price, lots.img, lots.expiry_date, categories.name AS category,
+SELECT lots.id, lots.name, lots.first_price, lots.img, lots.expiry_date, categories.name AS category,
        CASE
             WHEN (SELECT MAX(price) FROM bets WHERE bets.lot_id = lots.id) != 0 THEN (SELECT MAX(price) FROM bets WHERE bets.lot_id = lots.id)
             ELSE lots.first_price
@@ -37,49 +38,6 @@ SQL;
         return null;
     }
 }
-
-
-
-function price(int $price) : string {
-if (ceil($price) < 1000) {
-    return $price . " ₽";
-}
-else{
-    return number_format($price, 0, ',', ' ') . " ₽";
-    }
-}
-
-function include_template($name, array $data = []) {
-    $name = 'templates/' . $name;
-    $result = '';
-
-    if (!is_readable($name)) {
-        return $result;
-    }
-
-    ob_start();
-    extract($data);
-    require $name;
-
-    $result = ob_get_clean();
-
-    return $result;
-}
-
-function timer($time){
-    $diff = strtotime($time) - time();
-
-    $hours = floor($diff / 60 / 60); // перевод в часы с округлением вниз
-    $hours = str_pad ($hours, 2, "0", STR_PAD_LEFT); // добавляем 0 перед числом, если число меньше 2 знаков
-
-    $minutes = floor(($diff - ($hours * 60 * 60)) / 60); // получаем минуты
-    $minutes = str_pad ($minutes, 2, "0", STR_PAD_LEFT); // добавляем 0 перед числом, если число меньше 2 знаков
-
-    $timer = $hours . ':' . $minutes;
-
-    return $timer;
-}
-
 
 $page_content = include_template('main.php',[
     'categories' => $categories,
