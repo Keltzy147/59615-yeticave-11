108609ce19fbf758897a11f58e85d2c0f8cddbe6
Vafin Marat <56917627+Keltzy147@users.noreply.github.com>
2019-11-25 00:23:24 +0300

    Футер и хедер шаблонизация

---
+++ templates/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title><?= $title ?></title>
+	<link href="/yeticave/css/normalize.min.css" rel="stylesheet">
+	<link href="/yeticave/css/style.css" rel="stylesheet">
+</head>
+<body>
