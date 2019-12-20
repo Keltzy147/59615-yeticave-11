e5519162227ec91e4338dc16aaa8f1b760e4d3c1
Vafin Marat <56917627+Keltzy147@users.noreply.github.com>
2019-12-20 20:51:55 +0300

    upd 8-2

--- templates/lot.php
+++ templates/lot.php
@@ -3,7 +3,7 @@
           <div class="lot-item__content">
             <div class="lot-item__left">
               <div class="lot-item__image">
-                <img src="<?= $lot['img'] ?>" width="730" height="548" alt="<?= $lot['name'] ?>">
+                <img src="/yeticave/<?= $lot['img'] ?>" width="730" height="548" alt="<?= $lot['name'] ?>">
               </div>
               <p class="lot-item__category">Категория: <span><?= $lot['category'] ?></span></p>
               <p class="lot-item__description"><?= $lot['description'] ?></p>
